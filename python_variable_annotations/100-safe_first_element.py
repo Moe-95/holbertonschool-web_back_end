fdc7f5c8ef0f6a55f37f12ff1ccbcec569b4f10c
Moe-95 <mhammad_fahs@hotmail.com>
2024-04-28 16:04:38 +0300

    12

--- python_variable_annotations/100-safe_first_element.py
+++ python_variable_annotations/100-safe_first_element.py
@@ -1,16 +1,13 @@
 #!/usr/bin/env python3
-""" Augment the following code with the correct duck-typed annotations:
-    The types of the elements of the input are not know
-    def safe_first_element(lst):
-        if lst:
-            return lst[0]
-        else:
-            return None """
-from typing import Sequence, Any, Union
+""" 10. Duck typing - first element of a sequence
+"""
+
+from typing import Any, Sequence, Union
 
 
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
-    """ Duck typing - first element of a sequence """
+    """ Gets first element safely.
+    """
     if lst:
         return lst[0]
     else:
--- python_variable_annotations/101-safely_get_value.py
+++ python_variable_annotations/101-safely_get_value.py
@@ -1,22 +1,16 @@
 #!/usr/bin/env python3
-""" Given the parameters and the return values,
-    add type annotations to the function
-    Hint: look into TypeVar
-    def safely_get_value(dct, key, default = None):
-        if key in dct:
-            return dct[key]
-        else:
-            return default """
-from typing import Mapping, Any, Union, TypeVar
+""" 11. More involved type annotations
+"""
+
+from typing import Any, Mapping, TypeVar, Union
+
 T = TypeVar('T')
 
 
-def safely_get_value(
-        dct: Mapping,
-        key: Any,
-        default: Union[T, None] = None
-        ) -> Union[Any, T]:
-    """ More involved type annotations """
+def safely_get_value(dct: Mapping, key: Any,
+                     default: Union[T, None] = None) -> Union[Any, T]:
+    """ Gets value safely.
+    """
     if key in dct:
         return dct[key]
     else:
